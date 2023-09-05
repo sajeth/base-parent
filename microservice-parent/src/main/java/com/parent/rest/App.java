fa59a990384402c8dbd1a01503e5aa6a2183d16d
Sajeth <sajethbabu@gmail.com>
2023-09-04 22:45:19 -0300

    initial commit

---
+++ microservice-parent/src/main/java/com/parent/rest/App.java
@@ -0,0 +1,13 @@
+package com.parent.rest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
